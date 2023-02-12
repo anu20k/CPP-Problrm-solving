c684c8699b42ea061036acad759b46101678cb89
anu20k <103042185+anu20k@users.noreply.github.com>
2023-02-12 17:45:50 +0530

    Create Maximize sum(arr[i]*i) of an Array.cpp

---
+++ Maximize sum(arr[i]*i) of an Array.cpp
@@ -0,0 +1,35 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+    public:
+    int Maximize(int a[],int n)
+    {
+        long long ans=0;
+        sort(a, a+n);
+        for(int i=0;i<n;i++){
+            ans +=(long) a[i]*i;
+        }
+        long long ans1 = ans % 1000000007;
+        return ans1;
+    }
+};
+
+//{ Driver Code Starts.
+int main(){
+
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        int a[n];
+        for(int i=0;i<n;i++)
+            cin>>a[i];
+        Solution ob;
+        cout<<ob.Maximize(a,n)<<endl;
+    }
+}
+// } Driver Code Ends
