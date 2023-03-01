04f81006a4bba7cf0c78dbb1cd9fd37021ff3146
Anuja Kotkar <103042185+anu20k@users.noreply.github.com>
2023-03-02 01:53:51 +0530

    Create Square root of a number.cpp

---
+++ Square root of a number.cpp
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+  
+
+// } Driver Code Ends
+// Function to find square root
+// x: element to find square root
+class Solution{
+  public:
+    long long int floorSqrt(long long int x) 
+    {
+        // Your code goes here   
+        
+        if(x==0 || x==1){
+            return x;
+        }
+        
+        int i=1;
+        int result=1;
+        while(result<=x){
+            i++;
+            result = i* i;
+        }
+        return i-1;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		long long n;
+		cin>>n;
+		Solution obj;
+		cout << obj.floorSqrt(n) << endl;
+	}
+    return 0;   
+}
+
+// } Driver Code Ends
