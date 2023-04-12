3c5234223be2351f72f2675426819834ff90fd44
Anuja Kotkar <103042185+anu20k@users.noreply.github.com>
2023-04-12 09:50:43 +0530

    Create Duplicate Elements.cpp

---
+++ Duplicate Elements.cpp
@@ -0,0 +1,81 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class Array
+{
+public:
+    template <class T>
+    static void input(vector<T> &A,int n)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            scanf("%d ",&A[i]);
+        }
+    }
+ 
+    template <class T>
+    static void print(vector<T> &A)
+    {
+        for (int i = 0; i < A.size(); i++)
+        {
+            cout << A[i] << " ";
+        }
+        cout << endl;
+    }
+};
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    void SortedDuplicates(int n, vector<int> &arr) {
+        // code here
+        
+        map<int, int>mp;
+        int flag=0;
+        
+        for(int i=0;i<n;i++){
+            mp[arr[i]]++;
+        }
+        
+        for(auto it:mp){
+            
+            if(it.second>1){
+                cout<<it.first<<" ";
+                flag=1;
+            }
+        }
+        
+        if(flag==0){
+            cout<<-1;
+        }
+        cout<<endl;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    scanf("%d ",&t);
+    while(t--){
+        
+        int n; 
+        scanf("%d",&n);
+        
+        
+        vector<int> arr(n);
+        Array::input(arr,n);
+        
+        Solution obj;
+        
+        obj.SortedDuplicates(n, arr);
+        
+    }
+}
+
+// } Driver Code Ends
