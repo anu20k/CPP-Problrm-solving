987a24b0aa5fbfb24cf33f61f1a495a5ad1b1937
Anuja Kotkar <103042185+anu20k@users.noreply.github.com>
2023-04-14 12:00:21 +0530

    Create Find duplicates in an array.cpp

---
+++ Find duplicates in an array.cpp
@@ -0,0 +1,41 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+  public:
+    vector<int> duplicates(int arr[], int n) {
+        // code here
+          vector<int> v;
+        sort(arr,arr+n);
+        unordered_map<int,int> m;
+        for(int i=0;i<n;i++){ 
+            m[arr[i]]++;
+            
+            if(m[arr[i]]==2) v.push_back(arr[i]);
+        }
+        if(v.size()==0) v.push_back(-1);
+        return v;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t-- > 0) {
+        int n;
+        cin >> n;
+        int a[n];
+        for (int i = 0; i < n; i++) cin >> a[i];
+        Solution obj;
+        vector<int> ans = obj.duplicates(a, n);
+        for (int i : ans) cout << i << ' ';
+        cout << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
