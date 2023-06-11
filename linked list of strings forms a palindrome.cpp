f40843dd54ae0d5137f16e8c405cb474625b141f
Anuja Kotkar <103042185+anu20k@users.noreply.github.com>
2023-06-11 11:31:18 +0530

    Create linked list of strings forms a palindrome.cpp

---
+++ linked list of strings forms a palindrome.cpp
@@ -0,0 +1,110 @@
+//{ Driver Code Starts
+//Code by Shubham Joshi
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    string data;
+    Node* next;
+    
+    Node(string x){
+        data = x;
+        next = NULL;
+    }
+};
+
+Node *newNode(string str)
+{
+    Node *new_node = new Node(str);
+    new_node->next = NULL;
+    return new_node;
+}
+
+bool compute(struct Node* root);
+
+void print(Node *root)
+{
+    Node *temp = root;
+    
+    while(temp!=NULL)
+    {
+        cout<<temp->data<<endl;
+        temp=temp->next;
+    }
+}
+
+int main() {
+	// your code goes here
+	int T;
+	cin>>T;
+	while(T--)
+	{
+		int K;
+		cin>>K;
+		struct Node *head = NULL;
+                struct Node *temp = head;
+		
+		for(int i=0;i<K;i++){
+		string str="";
+		cin>>str;
+			if(head==NULL)
+			head=temp=new Node(str);
+			else
+			{
+				temp->next = new Node(str);
+				temp=temp->next;
+			}
+		}
+		if(compute(head))
+			cout<<"True"<<endl;
+		else 
+			cout<<"False"<<endl;
+	}
+	return 0;
+}
+// } Driver Code Ends
+
+
+/*
+The structure of linked list is the following
+struct Node
+{
+    string data;
+    Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+*/
+bool compute(Node* root)
+{
+   //Your code goes here
+   
+   string str;
+   
+   Node* temp = root;
+   
+   while(temp!=NULL){
+       
+       str+=temp->data;
+       temp=temp->next;
+   }
+   
+   int i=0;
+   int j=str.size()-1;
+   
+   while(i<j){
+       
+       if(str[i]==str[j]){
+           i++;
+           j--;
+       }
+       else{
+           return false;
+       }
+   }
+   return true;
+}
