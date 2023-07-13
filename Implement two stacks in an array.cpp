3767cd2bb9e5f443ddec081c466fedd2fb378223
Anuja Kotkar <103042185+anu20k@users.noreply.github.com>
2023-07-14 00:12:59 +0530

    Create Implement two stacks in an array.cpp

---
+++ Implement two stacks in an array.cpp
@@ -0,0 +1,112 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+
+
+class twoStacks
+{
+    int *arr;
+    int size;
+    int top1, top2;
+    public:
+    
+    twoStacks(int n=100)
+    {
+        size = n; 
+        arr = new int[n]; 
+        top1 = -1; 
+        top2 = size;
+    }
+    
+    stack<int>st, st1;
+ 
+    //Function to push an integer into the stack1.
+    void push1(int x)
+    {
+        st.push(x);
+    }
+    
+    //Function to push an integer into the stack2.
+    void push2(int x)
+    {
+        st1.push(x);
+       
+    }
+    
+    //Function to remove an element from top of the stack1.
+    int pop1()
+    {
+        int ans;
+        if(!st.empty()){
+            ans= st.top();
+            st.pop();
+        }
+        else{
+            return -1;
+        }
+        
+        return ans;
+    }
+    
+    //Function to remove an element from top of the stack2.
+    int pop2()
+    {
+        int ans;
+       if(!st1.empty()){
+            ans= st1.top();
+            st1.pop();
+        }
+        else{
+            return -1;
+        }
+        
+        return ans;
+    }
+};
+
+
+
+//{ Driver Code Starts.
+
+int main()
+{
+
+    int T;
+    cin>>T;
+    while(T--)
+    {
+        twoStacks *sq = new twoStacks();
+
+        int Q;
+        cin>>Q;
+        while(Q--){
+        int stack_no;
+        cin>>stack_no;
+        int QueryType=0;
+        cin>>QueryType;
+        
+        if(QueryType==1)
+        {
+            int a;
+            cin>>a;
+            if(stack_no ==1)
+            sq->push1(a);
+            else if(stack_no==2)
+            sq->push2(a);
+        }else if(QueryType==2){
+        	if(stack_no==1)
+            cout<<sq->pop1()<<" ";
+            else if(stack_no==2)
+            cout<<sq->pop2()<<" ";
+
+        }
+        }
+        cout<<endl;
+    }
+}
+
+// } Driver Code Ends
